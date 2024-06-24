597e3e9541f3c378713f8908eeb42756a7d98950
jvictordev1 <josevictorcruzrb@gmail.com>
2024-06-24 17:31:19 -0300

    feat: animations on landing page

--- src/routes/aboutPage/page.tsx
+++ src/routes/aboutPage/page.tsx
@@ -1,15 +1,56 @@
+import { Variants, motion } from "framer-motion";
 import Footer from "../../components/Footer";
 
 export default function AboutPage() {
+  const sectionVariants: Variants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: { duration: 0.8, delayChildren: 0.3, staggerChildren: 0.6 },
+    },
+  };
+  const textVariant: Variants = {
+    hidden: {
+      opacity: 0,
+      y: -20,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.8 },
+    },
+  };
+  const imagesVariant: Variants = {
+    hidden: {
+      opacity: 0,
+      y: -20,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.8 },
+    },
+  };
   return (
     <>
-      <section className="pt-28 scroll-smooth font-bold flex flex-col gap-32 bg-about-background-color">
+      <motion.section className="pt-28 scroll-smooth font-bold flex flex-col gap-32 bg-about-background-color">
         <section className="flex flex-col">
-          <div className="flex justify-center flex-col items-center gap-5">
-            <p className="w-1/3 text-center text-2xl text-zinc-800">
-              <span className="text-primary-red font-eckmann text-3xl">
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            variants={sectionVariants}
+            viewport={{ once: true }}
+            className="flex justify-center flex-col items-center gap-5"
+          >
+            <motion.p
+              variants={textVariant}
+              className="w-1/3 text-center text-2xl text-zinc-800"
+            >
+              <motion.span className="text-primary-red font-eckmann text-3xl">
                 EDEL
-              </span>{" "}
+              </motion.span>{" "}
               is a DJ/producer born in{" "}
               <span className="text-primary-red">Bahia, Brazil.</span> His
               musical journey began at home, where his mother introduced him to{" "}
@@ -18,19 +59,31 @@ export default function AboutPage() {
               </span>{" "}
               and at the age of 10, his father gifted him his{" "}
               <span className="text-primary-red">first guitar.</span>
-            </p>
-            <div className="flex justify-center w-full bg-about-page-bg bg-no-repeat bg-center bg-contain">
+            </motion.p>
+            <motion.div
+              variants={imagesVariant}
+              className="flex justify-center w-full bg-about-page-bg bg-no-repeat bg-center bg-contain"
+            >
               <img
                 className="max-w-md"
                 src="/images/img1-about.png"
-                alt="EDEL playing a track with a view from the ocean at the background."
+                alt="EDEL playing a track with a view from of ocean in the background."
               />
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         </section>
         <section className="flex flex-col">
-          <div className="flex justify-center flex-col items-center gap-5">
-            <p className="w-1/3 text-center text-2xl font-bold text-zinc-800">
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            variants={sectionVariants}
+            viewport={{ once: true }}
+            className="flex justify-center flex-col items-center gap-5"
+          >
+            <motion.p
+              variants={textVariant}
+              className="w-1/3 text-center text-2xl font-bold text-zinc-800"
+            >
               Started{" "}
               <span className="text-primary-red">producing music at 16</span>{" "}
               and over time has had several opportunities to perform at
@@ -42,36 +95,44 @@ export default function AboutPage() {
               <span className="text-primary-red">
                 first independent single.
               </span>
-            </p>
+            </motion.p>
             <div className="flex gap-20">
               <img
                 className="max-w-md"
                 src="/images/img2-about.png"
-                alt="EDEL playing a track with a view from the ocean at the background."
+                alt="Close image from EDEL's DJ equipment."
               />
               <img
                 className="max-w-md"
                 src="/images/img3-about.png"
-                alt="EDEL playing a track with a view from the ocean at the background."
+                alt="EDEL smiling while uses his DJ equipment with a view of the ocean in the background. ."
               />
             </div>
-          </div>
+          </motion.div>
         </section>
         <section className="flex flex-col gap-5">
-          <div className="flex justify-center flex-col items-center">
-            <p className="w-1/3 text-center text-2xl font-bold text-zinc-800">
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            className="flex justify-center flex-col items-center"
+          >
+            <motion.p
+              variants={textVariant}
+              className="w-1/3 text-center text-2xl font-bold text-zinc-800"
+            >
               Today, continues to refine his skills as a DJ and producer,
               bringing innovative contributions to the{" "}
               <span className="text-primary-red">
                 melodic house/afro scene,
               </span>{" "}
               heavily influenced by his hometown.
-            </p>
-          </div>
+            </motion.p>
+          </motion.div>
           <section className="h-dvh bg-edel-playing-about-bg bg-no-repeat bg-cover"></section>
         </section>
         <Footer />
-      </section>
+      </motion.section>
     </>
   );
 }
--- src/routes/landingPage/page.tsx
+++ src/routes/landingPage/page.tsx
@@ -1,40 +1,94 @@
+import { Variants, motion } from "framer-motion";
 import { AiOutlineSpotify } from "react-icons/ai";
 import { FaInstagram } from "react-icons/fa6";
 import { FiYoutube } from "react-icons/fi";
 import { RiSoundcloudLine } from "react-icons/ri";
 
 export default function LandingPage() {
+  const iconsListVariants: Variants = {
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        bounce: 0,
+        duration: 0.7,
+        delayChildren: 1,
+        staggerChildren: 0.2,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      y: 20,
+      transition: {
+        type: "spring",
+        bounce: 0,
+        duration: 0.3,
+      },
+    },
+  };
+  const iconsVariants: Variants = {
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { type: "spring", stiffness: 300, damping: 24 },
+    },
+    hidden: { opacity: 0, y: 100, transition: { duration: 0.2 } },
+  };
   return (
     <section className="h-screen bg-first-page-bg bg-no-repeat bg-cover bg-zinc-900">
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="w-1/3">
-          <img src="/images/del-logo.png" alt="EDELMUZIK logo" />
-          <div className="flex justify-center w-full mt-5 gap-8">
-            <a
-              title="Instagram"
-              target="_blank"
-              href="https://www.instagram.com/edelmuzik/"
-            >
-              <FaInstagram className="size-12" />
-            </a>
-            <a
-              title="Soundcloud"
-              target="_blank"
-              href="https://soundcloud.com/edelmuzik"
-            >
-              <RiSoundcloudLine className="size-12" />
-            </a>
-            <a
-              title="Spotify"
-              target="_blank"
-              href="https://open.spotify.com/artist/71FZZiU3aazM8r1Bm1NkCu"
-            >
-              <AiOutlineSpotify className="size-12" />
-            </a>
-            <a title="Youtube" target="_blank" href="#">
-              <FiYoutube className="size-12" />
-            </a>
-          </div>
+          <motion.img
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 1,
+              delay: 0.3,
+              ease: [0, 0.71, 0.2, 1.02],
+            }}
+            src="/images/del-logo.png"
+            alt="EDELMUZIK logo"
+          />
+          <motion.ul
+            initial="hidden"
+            animate="visible"
+            variants={iconsListVariants}
+            className="flex justify-center w-full mt-5 gap-8"
+          >
+            <motion.li variants={iconsVariants}>
+              <a
+                title="Instagram"
+                target="_blank"
+                href="https://www.instagram.com/edelmuzik/"
+              >
+                <FaInstagram className="size-12" />
+              </a>
+            </motion.li>
+            <motion.li variants={iconsVariants}>
+              <a
+                title="Soundcloud"
+                target="_blank"
+                href="https://soundcloud.com/edelmuzik"
+              >
+                <RiSoundcloudLine className="size-12" />
+              </a>
+            </motion.li>
+            <motion.li variants={iconsVariants}>
+              <a
+                title="Spotify"
+                target="_blank"
+                href="https://open.spotify.com/artist/71FZZiU3aazM8r1Bm1NkCu"
+              >
+                <AiOutlineSpotify className="size-12" />
+              </a>
+            </motion.li>
+            <motion.li variants={iconsVariants}>
+              <a title="Youtube" target="_blank" href="#">
+                <FiYoutube className="size-12" />
+              </a>
+            </motion.li>
+          </motion.ul>
         </div>
       </div>
     </section>
